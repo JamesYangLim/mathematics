8d229a8f45583859c9173fb47ba0f8be043cc5d2
James Lim <james.lim@manufacturingintelligence.com.au>
2019-07-11 08:29:31 +0800

    Remove Vec.h

--- geometry/Vector.h
+++ geometry/Vector.h
@@ -9,21 +9,48 @@ Vector.h
 namespace geom
 {
     template <typename T, size_t Dimension>
-    struct Vector : std::array<T, Dimension>
+    struct Vector
     {
+        std::array<T, Dimension> v;
+
         template <typename... Values>
-        Vector(Values... values) : array({ std::forward<Values>(values)... })
+        Vector(Values... values) : v({ std::forward<Values>(values)... })
         {}
 
         friend std::ostream& operator<<(std::ostream& os, const Vector& v);
-        Vector operator+(const Vector& rhs) const;
+        friend Vector operator+(const Vector& rhs);
         Vector operator-(const Vector& rhs) const;
         Vector operator*(T v) const;
         Vector operator/(T v) const;
+        Vector& operator+=(const Vector& rhs) const;
+        Vector& operator-=(const Vector& rhs) const;
+        Vector& operator*=(T rhs) const;
+        Vector& operator/=(T rhs) const;
         Vector DotProduct(const Vector& rhs) const;
         Vector CrossProduct(const Vector& rhs) const;
     };
 
+    template <typename T>
+    struct Vector2 : Vector<T, 2>
+    {
+        Vector2(T x, T y) : Vector<T, 2>(x, y)
+        {}
 
+        T X() const { return this->v[0]; }
+        T Y() const { return this->v[1]; };
+
+    };
+
+    template <typename T>
+    struct Vector3 : Vector<T, 3>
+    {
+        Vector3(T x, T y, T z) : Vector<T, 3>(x, y, z)
+        {}
+
+        T X() const { return this->v[0]; }
+        T Y() const { return this->v[1]; };
+        T Z() const { return this->v[2]; };
+
+    };
 
 }
--- geometryTest/test.cpp
+++ geometryTest/test.cpp
@@ -7,6 +7,9 @@
 int main()
 {
     geom::Vector<int, 3> v;
+    geom::Vector<int, 3> v1(1, 2, 3);
+    geom::Vector2<int> v2{ 1,2 };
+    std::cout << v2.X() << " " << v2.Y() << "\n";
 
     std::cout << "Press a key...";
     _getch();
